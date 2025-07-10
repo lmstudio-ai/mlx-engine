452668979bb7749636936fc9851883716b88921e
Neil Mehta <neil@lmstudio.ai>
2025-07-10 17:41:25 -0400

    Add support for ernie 4.5 moe

---
+++ mlx_engine/external/models/ernie4_5_moe/README.md
@@ -0,0 +1,1 @@
+These files were retrieved from https://huggingface.co/mlx-community/ERNIE-4.5-21B-A3B-PT-4bit/tree/6a55392c9930d43543bfa0a8ac6b67985bf4bd1d
--- mlx_engine/utils/register_models.py
+++ mlx_engine/utils/register_models.py
@@ -2,8 +2,12 @@
 
 from transformers import AutoTokenizer
 from mlx_engine.external.models.ernie4_5.configuration_ernie4_5 import Ernie4_5_Config
+from mlx_engine.external.models.ernie4_5_moe.configuration_ernie4_5_moe import (
+    Ernie4_5_MoeConfig,
+)
 from mlx_engine.external.models.ernie4_5.tokenization_ernie4_5 import Ernie4_5_Tokenizer
 
 
 def register_models():
     AutoTokenizer.register(Ernie4_5_Config, Ernie4_5_Tokenizer)
+    AutoTokenizer.register(Ernie4_5_MoeConfig, Ernie4_5_Tokenizer)
