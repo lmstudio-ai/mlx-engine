a59fa771bb220dc8e7eb99030b2030762bb1a931
Christian Zhou-Zheng <christian@lmstudio.ai>
2025-08-18 14:51:40 -0400

    it's only the 1.6B???????????????

--- mlx_engine/model_kit/vision_add_ons/load_utils.py
+++ mlx_engine/model_kit/vision_add_ons/load_utils.py
@@ -39,10 +39,11 @@ def load_and_parse_config(
     config.vision_config = vision_config_class.from_dict(config.vision_config)
     config.text_config = text_config_class.from_dict(config.text_config)
 
-    # hack for lfm2_vl
+    # hack for lfm2_vl 1.6B SPECIFICALLY (450M is fine???)
     if hasattr(config.text_config, "model_type") and "lfm2" in config.text_config.model_type:
-        config.vision_config.num_hidden_layers -= 1
-        config_dict["vision_config"]["num_hidden_layers"] -= 1
+        if config.vision_config.num_hidden_layers == 27:
+            config.vision_config.num_hidden_layers = 26
+            config_dict["vision_config"]["num_hidden_layers"] = 26
 
     return config, config_dict
 
