de47bcb0c56b9e2da5b2f95c053a237783da85f7
Christian Zhou-Zheng <christian@lmstudio.ai>
2025-08-18 13:14:49 -0400

    baseline tests: requires bumping several other versions

--- mlx_engine/utils/register_models.py
+++ mlx_engine/utils/register_models.py
@@ -1,13 +1,15 @@
 """Register local model-specific code to bypass enabling `trust_remote_code`."""
 
 from transformers import AutoTokenizer
-from mlx_engine.external.models.ernie4_5.configuration_ernie4_5 import Ernie4_5_Config
-from mlx_engine.external.models.ernie4_5_moe.configuration_ernie4_5_moe import (
-    Ernie4_5_MoeConfig,
-)
-from mlx_engine.external.models.ernie4_5.tokenization_ernie4_5 import Ernie4_5_Tokenizer
+# from mlx_engine.external.models.ernie4_5.configuration_ernie4_5 import Ernie4_5_Config
+# from mlx_engine.external.models.ernie4_5_moe.configuration_ernie4_5_moe import (
+#     Ernie4_5_MoeConfig,
+# )
+# from mlx_engine.external.models.ernie4_5.tokenization_ernie4_5 import Ernie4_5_Tokenizer
 
 
+# breaks after updating transformers to 4.54.0 which brings in native ernie support
 def register_models():
-    AutoTokenizer.register(Ernie4_5_Config, Ernie4_5_Tokenizer)
-    AutoTokenizer.register(Ernie4_5_MoeConfig, Ernie4_5_Tokenizer)
+    pass
+    # AutoTokenizer.register(Ernie4_5_Config, Ernie4_5_Tokenizer)
+    # AutoTokenizer.register(Ernie4_5_MoeConfig, Ernie4_5_Tokenizer)
--- tests/test_vision_models.py
+++ tests/test_vision_models.py
@@ -123,6 +123,20 @@ class TestVisionModels(unittest.TestCase):
             "mlx-community/pixtral-12b-4bit", prompt, text_only=True
         )
 
+    def test_lfm2_vl_vision(self):
+        """Test LFM2-VL 450M model"""
+        prompt = f"""<|startoftext|><|im_start|>user
+<image>{self.description_prompt}<|im_end|>
+<|im_start|>assistant"""
+        self.toucan_test_runner("mlx-community/LFM2-VL-450M-4bit", prompt)
+
+    def test_lfm2_vl_text_only(self):
+        """Test LFM2-VL 450M model"""
+        prompt = f"""<|startoftext|><|im_start|>user
+{self.text_only_prompt}<|im_end|>
+<|im_start|>assistant"""
+        self.toucan_test_runner("mlx-community/LFM2-VL-450M-4bit", prompt, text_only=True)
+
     @pytest.mark.heavy
     def test_mistral3_vision(self):
         prompt = f"<s>[INST]{self.description_prompt}[IMG][/INST]"
