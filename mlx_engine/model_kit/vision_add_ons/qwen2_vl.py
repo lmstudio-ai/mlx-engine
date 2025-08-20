2f0a9ed0b5da783bff685f4166e60dc003363a4c
Vincent Amato <vincentaamato@gmail.com>
2025-08-20 01:30:48 -0400

    Implemented load_qwen_vision_addon function + added should_pad arg to custom_resize for use with Qwen vision add-on

--- mlx_engine/model_kit/vision_add_ons/qwen2_vl.py
+++ mlx_engine/model_kit/vision_add_ons/qwen2_vl.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from mlx import nn
@@ -6,25 +7,10 @@ import mlx.core as mx
 from mlx_vlm.utils import prepare_inputs
 
 from mlx_engine.model_kit.vision_add_ons.base import BaseVisionAddOn
-from mlx_engine.model_kit.vision_add_ons.load_utils import (
-    load_and_parse_config,
-    maybe_apply_quantization,
-    load_and_filter_weights,
-    prepare_components,
-)
-from mlx_engine.model_kit.vision_add_ons.load_utils import (
-    load_processor,
-    sanitize_weights,
-)
-from mlx_engine.utils.image_utils import convert_to_pil
+from mlx_engine.model_kit.vision_add_ons.load_utils import load_qwen_vision_addon
+from mlx_engine.utils.image_utils import convert_to_pil, custom_resize
 
-
-class Qwen2VLVisionComponents(nn.Module):
-    """Container for Qwen2-VL vision components."""
-
-    def __init__(self, vision_tower: nn.Module):
-        super().__init__()
-        self.vision_tower = vision_tower
+logger = logging.getLogger(__name__)
 
 
 class Qwen2_VLVisionAddOn(BaseVisionAddOn):
@@ -65,46 +51,14 @@ class Qwen2_VLVisionAddOn(BaseVisionAddOn):
         # Store the combined model class for use in compute_embeddings
         self.CombinedModel = CombinedModel
 
-        # Load and parse configuration with correct classes
-        config, config_dict = load_and_parse_config(
+        self.vision_tower, self.config, self.processor = load_qwen_vision_addon(
             model_path=model_path,
             model_config_class=ModelConfigClass,
             vision_config_class=VisionConfigClass,
             text_config_class=TextConfigClass,
+            vision_tower_class=VisionTower,
+            logger=logger,
         )
-
-        # Create vision components container
-        components = Qwen2VLVisionComponents(
-            vision_tower=VisionTower(config.vision_config)
-        )
-
-        # Load and filter weights
-        vision_weights = load_and_filter_weights(
-            model_path=model_path,
-            components=components,
-        )
-
-        # Sanitize vision weights
-        vision_weights = sanitize_weights(
-            components.vision_tower.__class__, vision_weights, config.vision_config
-        )
-
-        # Apply quantization if specified
-        maybe_apply_quantization(
-            components=components,
-            config_dict=config_dict,
-            vision_weights=vision_weights,
-        )
-
-        # Prepare components
-        prepare_components(
-            components=components,
-            vision_weights=vision_weights,
-        )
-
-        self.vision_tower = components.vision_tower
-        self.config = config
-        self.processor = load_processor(model_path)
 
     def compute_embeddings(
         self,
@@ -125,7 +79,7 @@ class Qwen2_VLVisionAddOn(BaseVisionAddOn):
         images = convert_to_pil(images_b64)
 
         # Resize large images (without padding for multi-image support)
-        images = self._resize_images(images)
+        images = custom_resize(images, should_pad=False)
 
         # Prepare inputs using mlx_vlm's prepare_inputs
         inputs = prepare_inputs(
@@ -163,44 +117,3 @@ class Qwen2_VLVisionAddOn(BaseVisionAddOn):
 
         # Remove batch dimension
         return input_ids.squeeze(0), final_inputs_embeds.squeeze(0)
-
-    def _resize_images(self, images, max_size=(1000, 1000)):
-        """
-        Resize large images without padding (preserves multi-image support).
-
-        Args:
-            images: List of PIL images
-            max_size: Maximum dimensions (width, height)
-
-        Returns:
-            List of resized PIL images (no padding applied)
-        """
-        import PIL.Image
-        import logging
-
-        logger = logging.getLogger(__name__)
-        resized_images = []
-
-        for i, img in enumerate(images):
-            original_size = (img.width, img.height)
-
-            if img.width > max_size[0] or img.height > max_size[1]:
-                # Resize while maintaining aspect ratio
-                aspect_ratio = img.width / img.height
-                if img.width > img.height:
-                    new_width = max_size[0]
-                    new_height = int(new_width / aspect_ratio)
-                else:
-                    new_height = max_size[1]
-                    new_width = int(new_height * aspect_ratio)
-
-                img = img.resize((new_width, new_height), PIL.Image.LANCZOS)
-                logger.info(
-                    f"Image {i + 1}: Resized from {original_size} to {img.width}x{img.height}"
-                )
-            else:
-                logger.info(f"Image {i + 1}: No resize needed {original_size}")
-
-            resized_images.append(img)
-
-        return resized_images
