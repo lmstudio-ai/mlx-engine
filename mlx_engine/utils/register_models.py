90ee85556379666b7b9fb453fe2627d519ba9169
Neil Mehta <neil@lmstudio.ai>
2025-10-10 17:47:15 -0400

    Upgrade MLX requirements

--- mlx_engine/utils/register_models.py
+++ mlx_engine/utils/register_models.py
@@ -1,6 +1,7 @@
 """Register local model-specific code to bypass enabling `trust_remote_code`."""
 
 from transformers import AutoTokenizer, AutoProcessor
+import transformers.models.auto.processing_auto as processing_auto
 from mlx_engine.external.models.ernie4_5.configuration_ernie4_5 import Ernie4_5_Config
 from mlx_engine.external.models.ernie4_5_moe.configuration_ernie4_5_moe import (
     Ernie4_5_MoeConfig,
@@ -15,4 +16,7 @@ def register_models():
     # ref https://github.com/lmstudio-ai/mlx-engine/issues/211
     AutoTokenizer.register(Ernie4_5_Config, Ernie4_5_Tokenizer, exist_ok=True)
     AutoTokenizer.register(Ernie4_5_MoeConfig, Ernie4_5_Tokenizer, exist_ok=True)
+
+    # mlx-vlm is not compatible with the transformers version of lfm2
+    del processing_auto.PROCESSOR_MAPPING_NAMES["lfm2_vl"]
     AutoProcessor.register(Lfm2VlConfig, Lfm2VlProcessor, exist_ok=False)
